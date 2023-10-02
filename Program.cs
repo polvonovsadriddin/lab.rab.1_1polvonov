b47429b3c70121564098c6c4effaa0741bd406f1
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-10-03 00:15:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//высокий уровень10
+Console.Write("введите x:");
+double x = double.Parse(Console.ReadLine());
+Console.Write("введите y:");
+double y = double.Parse(Console.ReadLine());
+Console.Write("введите z:");
+double z = double.Parse(Console.ReadLine());
+double U = (Math.Exp(Math.Pow(x, 3))) + Math.Cos(x - 4) * Math.Cos(x - 4) / Math.Atan(x) + 5.2 * y;
+Console.WriteLine($"U={U:F2}");
